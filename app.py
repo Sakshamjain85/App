eefa06f60511a32a560b2dd3849449deed596652
Sakshamjain85 <121956368+Sakshamjain85@users.noreply.github.com>
2023-02-28 04:58:53 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.title(":red[Saksham] Jain :sunglasses:")
+st.subheader(":blue[Connect With Me:-]")
+st.title(":green[LinkedIn:-] ")
+if st.button('Click Here to Connect me On Linkedin'):
+    st.write('https://www.linkedin.com/in/saksham-jain-a694b9228/')
+    st.balloons()
+
+st.title(":green[GitHub:-] ")
+if st.button('Click Here to Visit my GitHub'):
+    st.write('https://github.com/Sakshamjain85')
+    st.balloons()
---
+++ pages/2_Dashboard_bike.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from matplotlib import image
+import pandas as pd
+import plotly.express as px
+import os
+
+# absolute path to this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+# absolute path of directory_of_interest
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+IMAGE_PATH = os.path.join(dir_of_interest, "images", "bike.jpg")
+DATA_PATH = os.path.join(dir_of_interest, "data", "bike.csv")
+
+st.title("Dashboard - car Data")
+
+img = image.imread(IMAGE_PATH)
+st.image(img)
+
+df = pd.read_csv(DATA_PATH)
+st.dataframe(df)
+
+species = st.selectbox("Select the Species:", df['Species'].unique())
